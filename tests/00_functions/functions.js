26ea8d0fe5bda146847819d7731dea4cf8783802
ArushaQM <aqmorgan2@gmail.com>
2023-03-19 19:45:23 -0400

    Finished MAX function

--- tests/00_functions/functions.js
+++ tests/00_functions/functions.js
@@ -1,8 +1,17 @@
 /* eslint-disable arrow-body-style */
 /* eslint-disable consistent-return */
-function max() {
-return 10
-}
+function max (num1 = 0, num2 = 0, num3 = 0) {
+  if ((num1 > num2) && (num1 > num3))
+  {
+    return num1
+  }else if((num3 > num2) && (num3 > num1))
+        {
+        return num3
+        }
+        else return num2
+  }
+  console.log(max(-40, -1, -15))
+  
 
 function addUnknown() {
 
